ff32be9ecb37ace2b52452afe061299a79535a81
Rose <Rose22@users.noreply.github.com>
2024-05-24 15:12:57 +0200

    added support for muOS and other cfw's
    by changing it into a mode of operation switch that can be expanded when needed
    
    also added a setting to specify a default folder, so the script can be run directly without having to use an argument

--- generate_m3u.py
+++ generate_m3u.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-# this script uses a keyword like "(Disc", (from something like "(Disc 1)" in the filename), to determine if a game is multi-disc.
-DISC_KEYWORD = "(Disc"
+SETTINGS = {
+    # the path to run the script against if no path was specified as an argument. leave blank to always require the commandline argument.
+    'default_path': '',
 
-# whether or not to skip generating m3u's for single-disc games
-SKIP_SINGLEDISC = True
+    # this script uses a keyword like "(Disc", (from something like "(Disc 1)" in the filename), to determine if a game is multi-disc.
+    'disc_keyword': '(Disc',
 
-# for minUI users, this'll create a folder per multi-disc game and put all the game files into it
-CREATE_FOLDERS = False
+    # whether or not to skip generating m3u's for single-disc games
+    'skip_singledisc': True,
+
+    # the mode to operate in. choose from:
+    #   samefolder: just create an m3u file for each multidisc game but leave the game files untouched
+    #   gamefolders: create a folder per game and move the multidisc files into them, create the m3u's inside the folders. this is used by minUI
+    #   subfolder: create one subfolder (such as .discs) and move all the game files into it, then create m3u's in the root folder. this is what muOS uses
+    'mode': 'subfolder',
+
+    # if you chose to use the subfolder mode, specify the name of that folder here
+    'subfolder_name': '.discs'
+}
 
 #############
 # -----------
@@ -17,11 +28,15 @@ import os
 import sys
 
 # the usual input checking
-if len(sys.argv) < 2:
-    print("usage: {cmd} <target folder>".format(cmd=sys.argv[0]))
-    sys.exit(0)
 
-target_path = sys.argv[1]
+if SETTINGS['default_path'] and len(sys.argv) < 2:
+    target_path = SETTINGS['default_path']
+else:
+    if len(sys.argv) < 2:
+        print("usage: {cmd} <target folder>".format(cmd=sys.argv[0]))
+        sys.exit(0)
+
+    target_path = sys.argv[1]
 
 if not os.path.isdir(target_path):
     print("invalid folder")
@@ -54,10 +69,10 @@ for filename in dirlist:
         continue
 
     if file_ext in ['chd', 'bin', 'cue', 'iso']:
-        gamename_split = filename_noext.split(DISC_KEYWORD)
+        gamename_split = filename_noext.split(SETTINGS['disc_keyword'])
         if len(gamename_split) == 1:
             # skip roms that don't have multiple discs, if preferred
-            if SKIP_SINGLEDISC:
+            if SETTINGS['skip_singledisc']:
                 continue
 
             pass
@@ -73,14 +88,26 @@ for filename in dirlist:
 for gamename, files in mapping.items():
     m3u_path = gamename+'.m3u'
 
-    if CREATE_FOLDERS:
-        if not os.path.isdir(gamename):
-            os.mkdir(gamename)
+    if SETTINGS['mode'] in ("gamefolders", "subfolder"):
+        match SETTINGS['mode']:
+            case "gamefolders":
+                target_folder = gamename
+            case "subfolder":
+                target_folder = SETTINGS['subfolder_name']
+
+        if not os.path.isdir(target_folder):
+            os.mkdir(target_folder)
 
         for filename in files:
-            os.rename(filename, gamename+'/'+filename)
+            os.rename(filename, target_folder+'/'+filename)
 
-        m3u_path = gamename+'/'+m3u_path
+        if SETTINGS['mode'] == "gamefolders":
+            m3u_path = target_folder+'/'+m3u_path
+
+    if SETTINGS['mode'] == "subfolder":
+        # modify the m3u so that it points to the hidden folder
+        for i in range(0, len(files)):
+            files[i] = target_folder+'/'+files[i]
 
     with open(m3u_path, 'w') as fh:
         fh.write("\n".join(files))
